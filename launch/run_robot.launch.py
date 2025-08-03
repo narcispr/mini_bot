685a0f74ba843346f8898b6972e315f68dc780e6
narcispr <narcispr@gmail.com>
2025-08-03 11:49:41 +0200

    Fix wheel base length and add launch that includes URDF from mini_bot_description

---
+++ launch/run_robot.launch.py
@@ -0,0 +1,47 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    pkg_path = get_package_share_directory('mini_bot_description')
+    urdf_path = os.path.join(pkg_path, 'urdf', 'mini_bot.urdf')
+    mini_bot_launch_path = os.path.join(get_package_share_directory('mini_bot'), 'launch', 'mini_bot.launch.py')
+
+    # Read the URDF file
+    with open(urdf_path, 'r') as f:
+        robot_description = f.read()
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(mini_bot_launch_path)
+        ),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{
+                'robot_description': robot_description,
+                'use_sim_time': LaunchConfiguration('use_sim_time')
+            }]
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', os.path.join(pkg_path, 'rviz', 'mini_bot.rviz')],
+        )
+    ])
